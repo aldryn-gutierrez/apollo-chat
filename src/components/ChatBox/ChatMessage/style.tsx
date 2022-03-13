64c1a992a7e1a263a33c632797c0cf091c6cba41
Aldryn Gutierrez <aldryn.gutierrez@gmail.com>
2022-03-13 12:33:53 +0800

    Remove Unused Style

--- src/components/ChatBox/ChatMessage/style.tsx
+++ src/components/ChatBox/ChatMessage/style.tsx
@@ -7,24 +7,6 @@ export const StyledDiv = styled.div<{ isSender: boolean }>`
   margin-bottom: 40px;
 `;
 
-export const ChatAvatarImage = styled.img`
-  width: 48px;
-  height: 48px;
-  -webkit-border-radius: 30px;
-  -moz-border-radius: 30px;
-  border-radius: 30px;
-`;
-
-ChatAvatarImage.defaultProps = {
-  src: "https://angular-test-backend-yc4c5cvnnq-an.a.run.app/Russell.png",
-};
-
-export const ChatNameDiv = styled.div`
-  font-size: 0.75rem;
-  color: #999999;
-  text-align: center;
-`;
-
 export const ChatTextDiv = styled.div<{ isSender: boolean }>`
   padding: 0.4rem 1rem;
   -webkit-border-radius: 4px;
